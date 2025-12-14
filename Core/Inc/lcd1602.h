878cf1a73e554dd188f84659fa53d176caea1a91
nanangimanmochammad-dotcom <nanangimanmochammad@madrasah.kemenag.go.id>
2025-12-14 21:58:02 +0800

    Update lcd1602.h

--- Core/Inc/lcd1602.h
+++ Core/Inc/lcd1602.h
@@ -8,14 +8,15 @@
 #ifndef INC_LCD1602_H_
 #define INC_LCD1602_H_
 
-#include "main.h" // 引入HAL库和引脚定义
+#include "main.h" // Introduce the HAL library and pin definitions
 
-// 核心功能函数
+// Core functional function
 void LCD_Init(void);
 void LCD_Clear(void);
 void LCD_SetCursor(uint8_t row, uint8_t col);
 void LCD_Print(char *str);
-void LCD_WriteChar(uint8_t data); // 写单个字符
-void LCD_CreateCustomChar(uint8_t location, uint8_t *charmap); // 创建自定义图标
+void LCD_WriteChar(uint8_t data); // Write a single character
+void LCD_CreateCustomChar(uint8_t location, uint8_t *charmap); //Create custom ICONS
 
 #endif /* INC_LCD1602_H_ */
+
